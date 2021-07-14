be87d1801feb3676434ce7873043c129cce3f8da
crlsdlgd <crlsdlgd1992@gmail.com>
2021-07-13 22:44:49 -0500

    listo mostrar objetivos y justificacion 3

--- PlanTitulacionEjb/src/java/ec/edu/uce/planTitulacion/ejb/utilities/FechaUtilToSql.java
+++ PlanTitulacionEjb/src/java/ec/edu/uce/planTitulacion/ejb/utilities/FechaUtilToSql.java
@@ -2,7 +2,7 @@ package ec.edu.uce.planTitulacion.ejb.utilities;
 
 import java.sql.Date;
 
-public class fechaUtilToSql {
+public class FechaUtilToSql {
     public static Date fechaUtilToSql(java.util.Date fecha) {
         Date fechaSql= new Date(fecha.getYear(),fecha.getMonth(),fecha.getDate());
         return fechaSql;
